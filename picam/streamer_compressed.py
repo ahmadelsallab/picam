6d1f28c6ded06a3f63e52bf16eff621a90423bbd
Ahmad El Sallab <ahmaad.elsallab@gmail.com>
2021-08-03 10:41:54 +0000

    Init

---
+++ picam/streamer_compressed.py
@@ -0,0 +1,83 @@
+import rclpy
+from rclpy.node import Node
+
+import cv2
+from cv_bridge import CvBridge
+
+from sensor_msgs.msg import CompressedImage
+
+import time
+
+class StreamerCompressed(Node):
+    def __init__(self) -> None:
+        super().__init__('streamer_compressed')
+        self.msg = CompressedImage()
+        self.publisher = self.create_publisher(CompressedImage, 'image_raw_compressed', 10)
+        # Capturing timer
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.frame_id = 0
+        self.vc = cv2.VideoCapture(0)
+        #self.vc.set(3,640)
+        #self.vc.set(4,480)
+        if not self.vc.isOpened(): assert('No camera')
+
+    def timer_callback(self):
+        res, img = self.capture()
+        if res:
+            self.publish(img)
+            self.frame_id += 1
+        else:
+            print('No frame')
+
+        
+    def capture(self):
+        res, frame = self.vc.read()
+        return res, frame
+    
+    def publish(self, img):
+        
+        msg = CvBridge().cv2_to_compressed_imgmsg(img, 'jpeg')
+        msg.header.frame_id = str(self.frame_id)
+        '''
+        msg.encoding = 'bgr8'
+        msg.width = 640
+        msg.height = 480
+        #msg = CvBridge().cv2_to_imgmsg(img, 'rgb8')
+        #msg.encoding = 'rgb8'
+        msg.header.frame_id = str(self.frame_id)
+        '''
+
+        '''
+        msg = Image()
+        msg.data = img
+        
+        msg.encoding = 'rgb8'
+        msg.width = 640
+        msg.height = 480
+        msg.header.frame_id = self.frame_id
+        msg.header.stamp = time.time()
+        '''    
+        self.publisher.publish(msg)
+        
+    def destroy_node(self) -> bool:
+        self.vc.release()
+        return super().destroy_node()
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    streamer = StreamerCompressed()
+
+    rclpy.spin(streamer)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    streamer.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
